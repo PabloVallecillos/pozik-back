becc76e96a3f40a5202124331dc5023cb7bfea01
PabloVallecillos <vallecillospablo@gmail.com>
2023-07-08 19:51:58 +0200

    feat: Added breeze and test phpunit.
    refs: #865cn5u9u

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpFoundation\Response as HttpStatus;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,14 +8,11 @@ use Symfony\Component\HttpFoundation\Response as HttpStatus;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    if (config('app.env') == 'local') {
-        return view('welcome');
-    }
-    abort(HttpStatus::HTTP_NOT_FOUND);
-});
+Route::get('/', fn () => [config('app.name') => config('pozik.version')]);
+
+require __DIR__.'/auth.php';
